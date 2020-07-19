10992a2c371b4d18507f59908e886dc3204606a1
Tuomo Oila <tuomo.oila@gmail.com>
2020-07-19 18:45:39 +0300

    refactor: move self image to its own component

---
+++ src/components/self-image.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
+import { graphql, useStaticQuery } from 'gatsby'
+import { FixedObject } from 'gatsby-image'
+
+interface SelfImageQueryProps {
+  file: { childImageSharp: { fixed: FixedObject } }
+}
+
+const SelfImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
+
+const StyledImage = styled(Img)`
+  border-radius: 50%;
+`
+
+const SelfImage: React.FC = () => {
+  const data = useStaticQuery<SelfImageQueryProps>(graphql`
+    query {
+      file(relativePath: { eq: "tuomo-oila.jpg" }) {
+        childImageSharp {
+          fixed(width: 125, height: 125) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <SelfImageContainer>
+      <StyledImage fixed={data.file.childImageSharp.fixed} />
+    </SelfImageContainer>
+  )
+}
+
+export default SelfImage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,20 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import styled from 'styled-components'
 
 import SquareTopRight from '../components/square-right-top'
 import SquareRightBottom from '../components/square-right-bottom'
 import SquareLeftBottom from '../components/square-left-bottom'
 import SquareLeftTop from '../components/square-left-top'
+import SelfImage from '../components/self-image'
 
-const SelfImageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-`
+const MainPage: React.FC = () => (
+  <React.Fragment>
+    <SquareLeftTop />
+    <SquareTopRight />
+    <SquareLeftBottom />
+    <SquareRightBottom />
 
-interface MainPageProps {
-  data: any // TODO improve typing
-}
-
-const StyledImage = styled(Img)`
-  border-radius: 50%;
-`
-
-const MainPage: React.FC<MainPageProps> = ({ data }) => {
-  return (
-    <React.Fragment>
-      <SquareLeftTop />
-      <SquareTopRight />
-      <SquareLeftBottom />
-      <SquareRightBottom />
-
-      <SelfImageContainer>
-        <StyledImage
-          fixed={data.file.childImageSharp.fixed}
-          style={{ borderRadius: '50%' }}
-        />
-      </SelfImageContainer>
-    </React.Fragment>
-  )
-}
+    <SelfImage />
+  </React.Fragment>
+)
 
 export default MainPage
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "tuomo-oila.jpg" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fixed(width: 125, height: 125) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`
